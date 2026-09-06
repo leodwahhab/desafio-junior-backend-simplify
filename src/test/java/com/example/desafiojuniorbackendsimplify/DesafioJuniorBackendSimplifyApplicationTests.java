f97ebe6b344b23b488e66667afa1586815967c53
leonardo wahhab <leonardo.wahhab@gmail.com>
2026-09-06 06:02:47 -0300

    initial commit

---
+++ src/test/java/com/example/desafiojuniorbackendsimplify/DesafioJuniorBackendSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiojuniorbackendsimplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJuniorBackendSimplifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
